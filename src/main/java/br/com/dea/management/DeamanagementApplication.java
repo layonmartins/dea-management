01e3042617f9a4654d535c05c153660e6f8e0142
layonmartins <layon.fonseca@eldorado.org.br>
2023-01-23 15:55:50 -0300

    Semana 2: create the user table and implement the SpringBoot Jpa

--- src/main/java/br/com/dea/management/DeamanagementApplication.java
+++ src/main/java/br/com/dea/management/DeamanagementApplication.java
@@ -1,13 +1,74 @@
 package br.com.dea.management;
 
+import br.com.dea.management.user.domain.User;
+import br.com.dea.management.user.repository.UserRepository;
+import br.com.dea.management.user.service.UserService;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+import java.util.Optional;
+
 @SpringBootApplication
-public class DeamanagementApplication {
+public class DeamanagementApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DeamanagementApplication.class, args);
 	}
 
+	@Autowired
+	private UserService userService;
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@PersistenceContext
+	private EntityManager entityManager;
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		//Deleting all Users
+		this.userRepository.deleteAll();
+
+		//Create some user
+		for (int i = 0; i < 5; i++) {
+			User u = new User();
+			u.setEmail("email " + i);
+			u.setName("Name " + i);
+			u.setLinkedin("linkedin " + i);
+			u.setPassword("password" + i);
+
+			this.userRepository.save(u);
+		}
+
+		//Loading all Users
+		List<User> users = this.userService.findAllUser();
+		users.forEach(u -> System.out.println("Name: " + u.getName()));
+
+		//Loading by query
+		Optional<User> loadingUserByName = this.userRepository.findByName("name 1");
+		System.out.println("User name 1 (From @Query) name: " + loadingUserByName.get().getName());
+
+		TypedQuery<User> q = entityManager.createNamedQuery("myQuery", User.class);
+		q.setParameter("name", "name 2");
+		User userFromNamedQuery = q.getResultList().get(0);
+		System.out.println("User name 2 (From NamedQuery) name : " + userFromNamedQuery.getName());
+
+		//Loading user by email
+		User loadedUser = this.userService.findUserByEmail("email 1");
+		System.out.println("User email 1 name: " + loadedUser.getName());
+
+		//Updating user name 1 linkedin
+		loadedUser.setLinkedin("new linkedin");
+		this.userRepository.save(loadedUser);
+		System.out.println("User email 1 name: " + loadedUser.getLinkedin());
+
+
+	}
 }
---
+++ src/main/java/br/com/dea/management/user/service/UserService.java
@@ -0,0 +1,35 @@
+package br.com.dea.management.user.service;
+
+import br.com.dea.management.exceptions.NotFoundException;
+import br.com.dea.management.user.domain.User;
+import br.com.dea.management.user.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findAllUser() {
+        return this.userRepository.findAll();
+    }
+
+    public User findUserByEmail(String email) {
+        Optional<User> user = this.userRepository.findByEmail(email);
+
+        if(user.isPresent()) {
+            return user.get();
+        }
+
+        throw new NotFoundException(User.class, email);
+
+        //return user.orElseThrow(() -> new NotFoundException(User.class, email));
+
+    }
+
+}
