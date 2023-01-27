d020e2c016660c35142843c7d045654ff33f7770
layonmartins <layon.fonseca@eldorado.org.br>
2023-01-27 17:00:55 -0300

    Semana3

--- src/main/java/br/com/dea/management/DeamanagementApplication.java
+++ src/main/java/br/com/dea/management/DeamanagementApplication.java
@@ -1,5 +1,7 @@
 package br.com.dea.management;
 
+import br.com.dea.management.student.domain.Student;
+import br.com.dea.management.student.repository.StudentRepository;
 import br.com.dea.management.user.domain.User;
 import br.com.dea.management.user.repository.UserRepository;
 import br.com.dea.management.user.service.UserService;
@@ -11,6 +13,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -27,6 +30,9 @@ public class DeamanagementApplication implements CommandLineRunner {
 	@Autowired
 	private UserRepository userRepository;
 
+	@Autowired
+	private StudentRepository studentRepository;
+
 	@PersistenceContext
 	private EntityManager entityManager;
 
@@ -36,39 +42,22 @@ public class DeamanagementApplication implements CommandLineRunner {
 		//Deleting all Users
 		this.userRepository.deleteAll();
 
-		//Create some user
-		for (int i = 0; i < 5; i++) {
+		//Create some students
+		for (int i = 0; i < 100; i++) {
 			User u = new User();
 			u.setEmail("email " + i);
 			u.setName("Name " + i);
 			u.setLinkedin("linkedin " + i);
 			u.setPassword("password" + i);
 
-			this.userRepository.save(u);
+			Student student = new Student();
+			student.setUniversity("UNI " + i);
+			student.setGraduation("Grad " + i);
+			student.setFinishDate(LocalDate.now());
+			student.setUser(u);
+
+			this.studentRepository.save(student);
 		}
-
-		//Loading all Users
-		List<User> users = this.userService.findAllUser();
-		users.forEach(u -> System.out.println("Name: " + u.getName()));
-
-		//Loading by query
-		Optional<User> loadingUserByName = this.userRepository.findByName("name 1");
-		System.out.println("User name 1 (From @Query) name: " + loadingUserByName.get().getName());
-
-		TypedQuery<User> q = entityManager.createNamedQuery("myQuery", User.class);
-		q.setParameter("name", "name 2");
-		User userFromNamedQuery = q.getResultList().get(0);
-		System.out.println("User name 2 (From NamedQuery) name : " + userFromNamedQuery.getName());
-
-		//Loading user by email
-		User loadedUser = this.userService.findUserByEmail("email 1");
-		System.out.println("User email 1 name: " + loadedUser.getName());
-
-		//Updating user name 1 linkedin
-		loadedUser.setLinkedin("new linkedin");
-		this.userRepository.save(loadedUser);
-		System.out.println("User email 1 name: " + loadedUser.getLinkedin());
-
 
 	}
 }
