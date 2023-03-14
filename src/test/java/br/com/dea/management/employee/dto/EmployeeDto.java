39bf375bfe6c6bf4628fb9d6117e7a479ac02b93
layonmartins <layon.fonseca@eldorado.org.br>
2023-03-14 17:17:59 -0300

    Semana7: Create EmployeeDto.java and PositionDto.jav

---
+++ src/test/java/br/com/dea/management/employee/dto/EmployeeDto.java
@@ -0,0 +1,54 @@
+package br.com.dea.management.employee.dto;
+
+
+import br.com.dea.management.employee.EmployeeType;
+import br.com.dea.management.employee.domain.Employee;
+import br.com.dea.management.position.domain.Position;
+import br.com.dea.management.position.dto.PositionDto;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class EmployeeDto {
+    private Long id;
+    private String name;
+    private String email;
+    private String linkedin;
+    private EmployeeType employeeType;
+    @JsonProperty("position")
+    private PositionDto positionDto;
+
+    public String getName() {
+        return name;
+    }
+
+    public static List<EmployeeDto> fromEmployees(List<Employee> employee) {
+        return employee.stream().map(empl -> {
+            EmployeeDto employeeDto = EmployeeDto.fromEmployee(empl);
+            return employeeDto;
+        }).collect(Collectors.toList());
+    }
+
+    public static EmployeeDto fromEmployee(Employee employee) {
+        EmployeeDto employeeDto = new EmployeeDto();
+        employeeDto.setId(employee.getId());
+        employeeDto.setName(employee.getUser().getName());
+        employeeDto.setEmail(employee.getUser().getEmail());
+        employeeDto.setLinkedin(employee.getUser().getLinkedin());
+        employeeDto.setEmployeeType(employee.getEmployeeType());
+
+        Position position = employee.getPosition();
+
+        employeeDto.setPositionDto(new PositionDto(position.getId(), position.getDescription(), position.getSeniority()));
+
+        return employeeDto;
+    }
+}
