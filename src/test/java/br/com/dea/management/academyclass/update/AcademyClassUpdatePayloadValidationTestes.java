8b49fdf70155f7de93098fe549b4124c1c7a08fd
layonmartins <layon.fonseca@eldorado.org.br>
2023-03-29 10:34:40 -0300

    Semana9: Created AcademyClassUpdatePayloadValidationTestes

--- src/test/java/br/com/dea/management/academyclass/update/AcademyClassUpdatePayloadValidationTestes.java
+++ src/test/java/br/com/dea/management/academyclass/update/AcademyClassUpdatePayloadValidationTestes.java
@@ -1,7 +1,10 @@
-package br.com.dea.management.academyclass.create;
+package br.com.dea.management.academyclass.update;
 
 import br.com.dea.management.AcademyTestUtils;
+import br.com.dea.management.academyclass.ClassType;
 import br.com.dea.management.academyclass.repository.AcademyClassRepository;
+import br.com.dea.management.employee.EmployeeTestUtils;
+import br.com.dea.management.employee.repository.EmployeeRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,17 +16,19 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.nio.charset.Charset;
+import java.time.LocalDate;
+import java.time.Month;
 
 import static org.hamcrest.Matchers.hasItem;
 import static org.hamcrest.Matchers.hasSize;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("test-mysql")
-public class AcademyClassCreationPayloadValidationTestes {
+public class AcademyClassUpdatePayloadValidationTestes {
 
     @Autowired
     private MockMvc mockMvc;
@@ -32,18 +37,24 @@ public class AcademyClassCreationPayloadValidationTestes {
     private AcademyClassRepository academyClassRepository;
 
     @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Autowired
     private AcademyTestUtils academyClassTestUtils;
+
+    @Autowired
+    private EmployeeTestUtils employeeTestUtils;
 
     public static final MediaType APPLICATION_JSON_UTF8 = new MediaType(MediaType.APPLICATION_JSON.getType(),
             MediaType.APPLICATION_JSON.getSubtype(), Charset.forName("utf8"));
 
     @Test
-    void whenPayloadRequiredFieldsAreMissing_thenReturn400AndTheErrors() throws Exception {
+    void whenUpdatePayloadRequiredFieldsAreMissing_thenReturn400AndTheErrors() throws Exception {
 
         String payload = "{}";
 
-        mockMvc.perform(post("/academy-class")
-                .contentType(APPLICATION_JSON_UTF8).content(payload))
+        mockMvc.perform(put("/academy-class/1")
+                        .contentType(APPLICATION_JSON_UTF8).content(payload))
                 .andExpect(status().isBadRequest())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$.message").exists())
@@ -60,16 +71,52 @@ public class AcademyClassCreationPayloadValidationTestes {
     }
 
     @Test
-    void whenPayloadInstructorIdRequiredDoesNotExists_thenReturn404() throws Exception {
+    void whenPayloadAndInstructorIdIsValidButAcademyClassRequiredDoesNotExists_thenReturn404() throws Exception {
+
+        this.academyClassRepository.deleteAll();
+        this.employeeRepository.deleteAll();
+
+        // created a valid employee to be the instructor
+        this.employeeTestUtils.createFakeEmployees(1);
+        Long instructorId = this.employeeRepository.findAll().get(0).getId();
 
         String payload = "{" +
                 "\"startDate\": \"2022-01-01\", " +
                 "\"endDate\": \"2023-01-01\", " +
                 "\"classType\" : \"DEVELOPER\", " +
-                "\"instructor\" : -10" + //employe of -10 not exists
+                "\"instructor\" : " + instructorId +
                 "}";
 
-        mockMvc.perform(post("/academy-class")
+        //academyClassId = 1 does not exists because we delete all academyClassRepository
+        mockMvc.perform(put("/academy-class/1")
+                        .contentType(APPLICATION_JSON_UTF8).content(payload))
+                .andExpect(status().isNotFound())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.message").exists())
+                .andExpect(jsonPath("$.details").isArray())
+                .andExpect(jsonPath("$.details", hasSize(1)));
+    }
+
+    @Test
+    void whenPayloadAndAcademyClassRequiredValidButInstructorIdRequiredDoesNotExists_thenReturn404() throws Exception {
+
+        this.academyClassRepository.deleteAll();
+        this.employeeRepository.deleteAll();
+
+        // created a valid academyClass
+        LocalDate startDate = LocalDate.of(2023, Month.JANUARY, 1);
+        LocalDate endDate = LocalDate.of(2024, Month.DECEMBER, 20);
+        this.academyClassTestUtils.createFakeClass(1, startDate, endDate, ClassType.DEVELOPER);
+        Long academyClassId = this.academyClassRepository.findAll().get(0).getId();
+
+        String payload = "{" +
+                "\"startDate\": \"2022-01-01\", " +
+                "\"endDate\": \"2023-01-01\", " +
+                "\"classType\" : \"DEVELOPER\", " +
+                "\"instructor\" : -1 " + // the instructorId -1 does not exist
+                "}";
+
+        mockMvc.perform(put("/academy-class/" + academyClassId)
                         .contentType(APPLICATION_JSON_UTF8).content(payload))
                 .andExpect(status().isNotFound())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
